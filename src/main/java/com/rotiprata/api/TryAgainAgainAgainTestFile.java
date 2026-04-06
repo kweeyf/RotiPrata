33a5b50e34a20c8499818d273fc18e878651da7d
hiyorinnn <hixyorin@gmail.com>
2026-04-06 18:35:33 +0800

    TryAgainAgainAgainTestFile

---
+++ src/main/java/com/rotiprata/api/TryAgainAgainAgainTestFile.java
@@ -0,0 +1,7 @@
+package com.rotiprata.api;
+
+public class TryAgainAgainAgainTestFile {
+    public TryAgainAgainAgainTestFile() {
+        System.out.println("CI is working!");
+    }
+}
--- src/main/java/com/rotiprata/api/TryAgainAgainTestFile.java
+++
@@ -1,7 +0,0 @@
-package com.rotiprata.api;
-
-public class TryAgainAgainTestFile {
-    public TryAgainAgainTestFile() {
-        System.out.println("CI is working!");
-    }
-}
